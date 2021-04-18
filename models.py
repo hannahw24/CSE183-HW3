49a6096bf9b34bfb5a85616ee550a169579e7949
Hannah Wong <hannah@Hannahs-MacBook-Pro.local>
2021-04-18 13:51:07 -0700

    Adding hw3 changes

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ This file defines the database models
 """
 
 import datetime
-from .common import db, Field, auth
+from .common import db, Field, auth, T
 from pydal.validators import *
 
 
@@ -23,8 +23,19 @@ def get_time():
 db.define_table(
     'bird',
     ### TODO: define the fields that are in the json.
-    Field('bird_count', 'integer'),
-    Field('seen_by', default=get_user_email()),
+    Field('bird_name', requires=IS_NOT_EMPTY()),
+    Field('bird_weight', 'integer', default=0, requires=IS_INT_IN_RANGE(0, 1e6)),
+    Field('bird_diet', requires=IS_NOT_EMPTY()),
+    Field('bird_habitat', requires=IS_NOT_EMPTY()),
+    Field('bird_count', 'integer', default=0, requires=IS_INT_IN_RANGE(0, 1e6)),
+    Field('seen_by', default=get_user_email),
 )
+
+db.bird.id.readable = False
+db.bird.seen_by.readable = db.bird.seen_by.writable = False
+db.bird.bird_name.label = T('Bird')
+db.bird.bird_weight.label = T('Weight')
+db.bird.bird_diet.label = T('Diet')
+db.bird.bird_habitat.label = T('Habitat')
 
 db.commit()
